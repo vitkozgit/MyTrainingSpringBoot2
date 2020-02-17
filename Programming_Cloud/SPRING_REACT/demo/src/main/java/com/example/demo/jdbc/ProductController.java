48231b066f4d391e8e7f621654a663a17136b4d8
Vitali Kozak <vitalikozak@Vitalis-iMac.local>
2020-02-17 20:45:23 +0100

    123

--- Programming_Cloud/SPRING_REACT/demo/src/main/java/com/example/demo/jdbc/ProductController.java
+++ Programming_Cloud/SPRING_REACT/demo/src/main/java/com/example/demo/jdbc/ProductController.java
@@ -1,9 +1,8 @@
 package com.example.demo.jdbc;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ProductController {
 
 	private ProductRepository productRepository;
