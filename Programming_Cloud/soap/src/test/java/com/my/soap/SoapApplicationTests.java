f74e1febd9f54748235439bd5a759f16890afc48
Vitali Kozak <vitalikozak@Vitalis-iMac.local>
2020-03-07 10:02:13 +0100

    zxvadsfvadfs

---
+++ Programming_Cloud/soap/src/test/java/com/my/soap/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
